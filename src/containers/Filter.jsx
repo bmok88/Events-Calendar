9e2d3b8ee11ce661e387d540057947cf3f823c2a
Brendan Mok <bmok88@gmail.com>
2017-09-12 18:35:52 -0700

    Refactor filter component to functional stateless

--- src/containers/Filter.jsx
+++ src/containers/Filter.jsx
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Filter extends Component {
-  render() {
-    const { event, filterTerms, addFilterTerm, removeFilterTerm } = this.props;
-    let checked = true;
-    if (filterTerms.indexOf(event) !== -1) {
-      checked = false;
-    }
-
-    return (
-      <div className="filter">
-        {event}
-        <input
-          className="filter-checkbox"
-          type="checkbox"
-          checked={checked}
-          value={event}
-          onChange={e => {
-            if (!filterTerms.includes(e.target.value)) {
-              addFilterTerm(e.target.value);
-            } else {
-              removeFilterTerm(e.target.value);
-            }
-            e.target.checked = false;
-            console.log(e.target.checked, 'input');
-          }}
-        />
-      </div>
-    );
+const Filter = ({ event, filterTerms, addFilterTerm, removeFilterTerm }) => {
+  let checked = true;
+  if (filterTerms.indexOf(event) !== -1) {
+    checked = false;
   }
-}
+
+  return (
+    <div className="filter">
+      {event}
+      <input
+        className="filter-checkbox"
+        type="checkbox"
+        checked={checked}
+        value={event}
+        onChange={e => {
+          if (!filterTerms.includes(e.target.value)) {
+            addFilterTerm(e.target.value);
+          } else {
+            removeFilterTerm(e.target.value);
+          }
+          e.target.checked = false;
+          console.log(e.target.checked, 'input');
+        }}
+      />
+    </div>
+  );
+};
 
 export default Filter;

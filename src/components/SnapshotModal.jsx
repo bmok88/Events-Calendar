adfb91835793bf0887ffbad2811f82b291b259a9
Brendan Mok <bmok88@gmail.com>
2017-09-17 18:21:35 -0700

    Add styling to snapshot modal

--- src/components/SnapshotModal.jsx
+++ src/components/SnapshotModal.jsx
@@ -2,14 +2,38 @@ import React from 'react';
 
 const SnapshotModal = ({ hideSnapshotModal }) => {
   return (
-    <div className="modal" id="snapshot-modal">
-      <div className="modal-content">
-        <h2>Name this snapshot</h2>
-        <input type="text" />
-        <button type="button" onClick={() => hideSnapshotModal()}>
-          Cancel
-        </button>
-        <button type="submit">Add</button>
+    <div
+      id="snapshot-modal"
+      tabIndex="-1"
+      role="dialog"
+      aria-hidden="true"
+      aria-labelledby="snapshot-modalLabel"
+      className="modal fade"
+    >
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Name This Snapshot</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <div className="col-12">
+              <input type="text" className="form-control" id="modal-input" />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button type="submit" aria-label="Add" className="btn btn-primary">
+              Add
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
